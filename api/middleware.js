033c1248398196781e96c83855420288c401450b
22026163-BraydenTan <22026163@myrp.edu.sg>
2024-07-15 23:38:39 +0800

    Create middleware.js

---
+++ api/middleware.js
@@ -0,0 +1,11 @@
+export default function middleware(req, event) {
+  return new Response(null, {
+    headers: {
+      'Referrer-Policy': 'origin-when-cross-origin',
+      'X-Frame-Options': 'DENY',
+      'X-Content-Type-Options': 'nosniff',
+      'X-DNS-Prefetch-Control': 'on',
+      'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload',
+    },
+  });
+}
